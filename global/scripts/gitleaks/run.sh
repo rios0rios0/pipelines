aba275ba3d487ed1bc11926f74976863ce1825c6
Felipe Rios <felipe.rios.silva@outlook.com>
2023-10-04 18:51:23 -0400

    fix(devops): corrected the cloning from itself in the Azure DevOps vendor
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

--- global/scripts/gitleaks/run.sh
+++ global/scripts/gitleaks/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-. "$SCRIPTS_DIR/global/scripts/shared/check-report-path.sh"
+export SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. "$SCRIPTS_DIR/global/scripts/shared/cleanup.sh"
 
 chmod -R 777 "$REPORT_PATH" # DinD approach needs this line
 export CONTAINER_PATH="/opt/src"
--- global/scripts/shared/check-report-path.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-if [ -z "$REPORT_PATH" ]; then
-  exit 100
-fi
-
-rm -rf "$REPORT_PATH"
-mkdir -p "$REPORT_PATH" # CI/CD variable

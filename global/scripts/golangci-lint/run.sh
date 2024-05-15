e2885bf4e5fa3cbd2d813d1fee49802f2321f069
Felipe Rios <felipe.rios.silva@outlook.com>
2024-05-15 17:28:59 -0400

    hotfix(go): changed `golangci-lint` to have 10 minutes of timeout
    Signed-off-by: Felipe Rios <felipe.rios.silva@outlook.com>

--- global/scripts/golangci-lint/run.sh
+++ global/scripts/golangci-lint/run.sh
@@ -22,7 +22,7 @@ wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/ins
 ./bin/golangci-lint run \
   --config "custom.json" \
   --color "always" \
-  --timeout "5m" \
+  --timeout "10m" \
   --print-resources-usage \
   --allow-parallel-runners \
   --max-issues-per-linter 0 \

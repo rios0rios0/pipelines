9a94b9388313cec67e8f0439e24a303c7495cf9a
k4yt3x <i@k4yt3x.com>
2024-08-07 19:46:26 +0000

    feat(golang): changed the GoLang linter configuration to use project-specific config
    Signed-off-by: k4yt3x <i@k4yt3x.com>

--- global/scripts/golangci-lint/run.sh
+++ global/scripts/golangci-lint/run.sh
@@ -4,23 +4,47 @@ if [ -z "$SCRIPTS_DIR" ]; then
   export SCRIPTS_DIR="$(echo $(dirname "$(realpath "$0")") | sed 's|\(.*pipelines\).*|\1|')"
 fi
 
-customJsonFile="custom.json"
-defaultJsonFile="$SCRIPTS_DIR/global/scripts/golangci-lint/.golangci.json"
+mergedYamlFile="merged.yml"
+defaultYamlFile="$SCRIPTS_DIR/global/scripts/golangci-lint/.golangci.yml"
 
-if [ -f ".golangci.json" ]; then
-  disableDefault=$(jq -r '.linters.disable' $defaultJsonFile)
-  disableSpecific=$(jq -r '.linters.disable' .golangci.json)
-  mergedDisable=$(echo "$disableDefault $disableSpecific" | jq -s 'add | unique')
-  mergedLinters=$(jq -s '.[0].linters + .[1].linters | del(.disable)' $defaultJsonFile .golangci.json)
-  result=$(jq --argjson disable "$mergedDisable" --argjson linters "$mergedLinters" -n '$linters * {"disable": $disable}')
-  echo "{\"linters\": $result}" > $customJsonFile
+if [ -f ".golangci.yml" ]; then
+  python3 - "$defaultYamlFile" "$mergedYamlFile" << EOF
+import sys, yaml
+
+default_yaml_path = sys.argv[1]
+merged_yaml_path = sys.argv[2]
+
+with open(default_yaml_path, "r") as default_config_file:
+    default_config = yaml.safe_load(default_config_file)
+with open(".golangci.yml", "r") as repo_config_file:
+    repo_config = yaml.safe_load(repo_config_file)
+
+merged_config = {
+    "linters": {
+        "enable-all": True,
+        "disable": list(
+            set(
+                default_config["linters"]["disable"]
+                + repo_config["linters"].get("disable", [])
+            )
+        ),
+    }
+}
+
+for linter in repo_config["linters"].get("enable", []):
+    if linter in merged_config["linters"]["disable"]:
+        merged_config["linters"]["disable"].remove(linter)
+
+with open(merged_yaml_path, "w") as merged_config_file:
+    yaml.dump(merged_config, merged_config_file)
+EOF
 else
-  cp "$defaultJsonFile" $customJsonFile
+  cp "$defaultYamlFile" $mergedYamlFile
 fi
 
 wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh
 ./bin/golangci-lint run \
-  --config "custom.json" \
+  --config "merged.yml" \
   --color "always" \
   --timeout "10m" \
   --print-resources-usage \
@@ -28,5 +52,6 @@ wget -O- -nv https://raw.githubusercontent.com/golangci/golangci-lint/master/ins
   --max-issues-per-linter 0 \
   --max-same-issues 0 ./... || EXIT_CODE=$?
 
-rm $customJsonFile
+rm $mergedYamlFile
 exit $EXIT_CODE
+

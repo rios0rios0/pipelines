eb4a93bb36b3702f18889b0ff54a2a77d6316475
Lucas Bruschi <lucas9bruschi@gmail.com>
2024-10-24 17:39:21 -0400

    fix(source-config-script-golang): source the script file to read the env vars

--- global/scripts/golang/test/run.sh
+++ global/scripts/golang/test/run.sh
@@ -8,9 +8,6 @@ fi
 export GOPATH="$(pwd)/.go" # the GOPATH must be absolute
 export PATH="$PATH:$GOPATH/bin" # this is a workaround to detect the new GOPATH
 
-export INIT_SCRIPT="config.sh"
-[ -f "$INIT_SCRIPT" ] && ./"$INIT_SCRIPT" || echo "The '$INIT_SCRIPT' file is not found, skipping..."
-
 touch coverage.xml
 
 # Find the directories to test
@@ -26,20 +23,13 @@ if [ -z "$directories" ]; then
   exit 1
 fi
 
-# Download and install docker for integration tests with test containers
-distro=$(grep '^NAME=' /etc/os-release | awk -F= '{print $2}' | tr -d '"')
-if ["$distro" != "Arch Linux" && [ "$distro" != "Gentoo" ]; then
-  echo "Installing docker"
-  curl -fsSL https://get.docker.com | bash
-else
-  echo "Skipping installation for unsupported distro"
-fi
-
 # Trim leading or trailing spaces
 directories=$(echo $directories | sed 's/^ *//;s/ *$//')
 echo "Testing code in the following directories: $directories"
 
+echo "Installing dependencies..."
 go install github.com/wadey/gocovmerge@latest
+go install github.com/boumenot/gocover-cobertura@latest
 
 # Run the tests
 go test -v -tags test,unit \
@@ -54,25 +44,10 @@ go test -p 1 -v -tags integration \
   -coverprofile=integration_coverage.txt \
   $directories
 
-test_exit_code=$?
-
 gocovmerge unit_coverage.txt integration_coverage.txt > coverage.txt
 
 rm unit_coverage.txt integration_coverage.txt
 
 go tool cover -func coverage.txt
-cover_exit_code=$?
-
-go install github.com/boumenot/gocover-cobertura@latest
-install_exit_code=$?
 
 gocover-cobertura < coverage.txt > coverage.xml
-cobertura_exit_code=$?
-
-# Exit with the highest exit code
-exit_code=$((test_exit_code > cover_exit_code ? test_exit_code : cover_exit_code))
-exit_code=$((exit_code > install_exit_code ? exit_code : install_exit_code))
-exit_code=$((exit_code > cobertura_exit_code ? exit_code : cobertura_exit_code))
-
-# Exit
-exit $exit_code

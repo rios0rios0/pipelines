a6870d2de7a72d7833f8304f1a28a1c317726bad
rios0rios0 <rios0rios0@outlook.com>
2026-02-11 14:36:44 -0300

    hotfix(makefile): corrected to have multiple targets instead of just one
    Signed-off-by: rios0rios0 <rios0rios0@outlook.com>

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -4,18 +4,28 @@
 #   SCRIPTS_DIR ?= $(HOME)/Development/github.com/rios0rios0/pipelines
 #   -include $(SCRIPTS_DIR)/makefiles/common.mk
 #
-# Targets provided: sast
+# Targets provided: setup codeql semgrep trivy hadolint gitleaks sast
 # Requires: SCRIPTS_DIR to be set. SEMGREP_LANGUAGE and CODEQL_LANGUAGE should be set by a language
 #           .mk file (e.g. golang.mk) or manually before including this file.
 
-.PHONY: setup sast
+.PHONY: setup codeql semgrep trivy hadolint gitleaks sast
 
 setup:
 	@curl -sSL https://raw.githubusercontent.com/rios0rios0/pipelines/main/clone.sh | bash
 
-sast:
+codeql:
 	@$(SCRIPTS_DIR)/global/scripts/tools/codeql/run.sh "$(CODEQL_LANGUAGE)"
+
+semgrep:
 	@$(SCRIPTS_DIR)/global/scripts/tools/semgrep/run.sh "$(SEMGREP_LANGUAGE)"
+
+trivy:
 	@$(SCRIPTS_DIR)/global/scripts/tools/trivy/run.sh
+
+hadolint:
 	@$(SCRIPTS_DIR)/global/scripts/tools/hadolint/run.sh
+
+gitleaks:
 	@$(SCRIPTS_DIR)/global/scripts/tools/gitleaks/run.sh
+
+sast: codeql semgrep trivy hadolint gitleaks

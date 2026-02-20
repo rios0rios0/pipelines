24b7c78c02df68c3c599a594274e3fc3ed0ed933
rios0rios0 <rios0rios0@outlook.com>
2026-02-20 13:48:34 -0300

    hotfix(python): added Safety target on Makefiles for Python
    Signed-off-by: rios0rios0 <rios0rios0@outlook.com>

--- makefiles/python.mk
+++ makefiles/python.mk
@@ -5,21 +5,24 @@
 #   -include $(SCRIPTS_DIR)/makefiles/common.mk
 #   -include $(SCRIPTS_DIR)/makefiles/python.mk
 #
-# Targets provided: lint, test
-# Also sets CODEQL_LANGUAGE=python, SEMGREP_LANGUAGE=python for the common.mk sast target.
+# Targets provided: lint, safety, test
+# Also sets CODEQL_LANGUAGE=python, SEMGREP_LANGUAGE=python for the common.mk SAST target.
 #
 # Prerequisites: PDM must be installed and the project must have a pyproject.toml.
 
 CODEQL_LANGUAGE ?= python
 SEMGREP_LANGUAGE ?= python
 
-.PHONY: lint test
+.PHONY: lint safety test
 
 lint:
 	@pdm run isort .
 	@pdm run black .
 	@pdm run flake8 .
 	@pdm run mypy .
+
+safety:
+	@pdm run safety
 
 test:
 	@pdm run test
